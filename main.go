41a0dc4d6d3d13db79404360ce3c3e6de5657066
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-05-12 02:27:29 +0900

    ADD: pkg: recip

--- main.go
+++ main.go
@@ -18,4 +18,5 @@ func main() {
 	fmt.Println(utils.Saturate(0.5))               // 0.5
 	fmt.Println(utils.Radians(1718.8733853924698)) // 30
 	fmt.Println(utils.Degrees(30))                 // rad × 180/π = 1718.8733853924698
+	fmt.Println(utils.Recip(2))
 }
--- pkg/utils/recip.go
+++ pkg/utils/recip.go
@@ -1,1 +1,5 @@
 package utils
+
+func Recip(x float64) float64 {
+	return 1 / x
+}
