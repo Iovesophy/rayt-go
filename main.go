b5200fcfaccf65ad3fcfff1b63ad3cc9609b358f
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-05-12 01:27:04 +0900

    ADD: double float rand 64

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"rayt-go/pkg/utils"
 )
 
@@ -10,4 +11,7 @@ func main() {
 	fmt.Println(utils.Pow3(0.1))
 	fmt.Println(utils.Pow4(0.1))
 	fmt.Println(utils.Pow5(0.1))
+	for i := 0; i < 3; i++ {
+		fmt.Println(rand.Float64())
+	}
 }
