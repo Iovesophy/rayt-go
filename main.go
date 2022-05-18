8f80ebb19753bcadc7ddccc14259af62490492b0
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-05-18 19:06:09 +0900

    Refactor- ppm: add create file

--- main.go
+++ main.go
@@ -1,42 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-type Image struct {
-	F string
-	X int
-	Y int
-	L int
+type RGB struct {
 	R int
 	G int
 	B int
 }
 
-func (image Image) Header() {
-	fmt.Printf("%s\n%d %d\n%d\n", image.F, image.X, image.Y, image.L)
+const (
+	HeaderFormat = "%s\n%d %d\n%d\n"
+	BodyFormat   = "%d %d %d\n"
+)
+
+type Image struct {
+	Format    string
+	X         int
+	Y         int
+	MaxBright int
+	Header    string
+	Body      strings.Builder
+	Color     RGB
 }
 
-func (image Image) Data() Image {
-	for j := 0; j < image.Y; j++ {
-		for i := 0; i < image.X; i++ {
-			r := float64(i) / float64(image.X)
-			g := float64(j) / float64(image.Y)
+func (img Image) CreateHeader() string {
+	return fmt.Sprintf(HeaderFormat, img.Format, img.X, img.Y, img.MaxBright)
+}
+
+func (img Image) CreateP3Data() Image {
+	img.Header = img.CreateHeader()
+	for j := 0; j < img.Y; j++ {
+		for i := 0; i < img.X; i++ {
+			r := float64(i) / float64(img.X)
+			g := float64(j) / float64(img.Y)
 			b := 0.2
-			image.R = int(255.99 * r)
-			image.G = int(255.99 * g)
-			image.B = int(255.99 * b)
-			fmt.Printf("%d %d %d\n", image.R, image.G, image.B)
+			img.Color.R = int(255.99 * r)
+			img.Color.G = int(255.99 * g)
+			img.Color.B = int(255.99 * b)
+			img.Body.WriteString(fmt.Sprintf(BodyFormat, img.Color.R, img.Color.G, img.Color.B))
 		}
 	}
-	return image
+	return img
+}
+
+func (img Image) CreateFile(filename string, header string, body string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteString(header)
+	if err != nil {
+		return err
+	}
+	_, err = file.WriteString(body)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
-	image := Image{}
-	image.F = "P3"
-	image.X = 200
-	image.Y = 100
-	image.L = 255
-	image.Header()
-	image.Data()
+	img := Image{}
+	img.Format = "P3"
+	img.X = 200
+	img.Y = 100
+	img.MaxBright = 255
+	result := img.CreateP3Data()
+
+	filename := "test.ppm"
+	img.CreateFile(filename, result.Header, result.Body.String())
 }
