077df0a79a8634615590c8e86e82d4567116e4e0
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-09-29 17:41:36 +0900

    FIX: change Vector definition

--- pkg/ray/ray.go
+++ pkg/ray/ray.go
@@ -1,6 +1,10 @@
 package ray
 
-import "github.com/golang/geo/r3"
+import (
+	"rayt-go/pkg/scene"
+
+	"github.com/golang/geo/r3"
+)
 
 type VertexPair struct {
 	Origin    r3.Vector
@@ -11,10 +15,10 @@ func New(origin r3.Vector, direction r3.Vector) VertexPair {
 	return VertexPair{Origin: origin, Direction: direction}
 }
 
-func PointAtParameter(t float64, r VertexPair) r3.Vector {
-	return r3.Vector{
-		X: r.Origin.X + t*r.Direction.X,
-		Y: r.Origin.Y + t*r.Direction.Y,
-		Z: r.Origin.Z + t*r.Direction.Z,
-	}
+func PointAtParameter(t float64, vertexpair VertexPair) r3.Vector {
+	return scene.NewVector(
+		vertexpair.Origin.X+t*vertexpair.Direction.X,
+		vertexpair.Origin.Y+t*vertexpair.Direction.Y,
+		vertexpair.Origin.Z+t*vertexpair.Direction.Z,
+	)
 }
