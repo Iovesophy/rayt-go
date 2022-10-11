008de09e680e6835ed1fbba171f0d5b22edef24f
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-10-11 17:11:50 +0900

    ADD: Fuzziness material

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Iovesophy/rayt-go/pkg/geometry"
 	"github.com/Iovesophy/rayt-go/pkg/image"
 	"github.com/Iovesophy/rayt-go/pkg/material"
@@ -21,11 +23,11 @@ func main() {
 	e.Depth = 0
 	e.Camera = camera.New(camera.Main(e.X, e.Y))
 	e.World = append(e.World,
-		geometry.NewSphere(r3.Vector{X: -1, Y: 0, Z: -1}, 0.5, material.NewMetal(scene.NewVector(0.8, 0.8, 0.8))),
-		geometry.NewSphere(r3.Vector{X: 0, Y: 0, Z: -1}, 0.5, material.NewLambertian(scene.NewVector(0.3, 0.8, 0.5))),
-		geometry.NewSphere(r3.Vector{X: 1, Y: 0, Z: -1}, 0.5, material.NewNormal(0.5)),
-		geometry.NewSphere(r3.Vector{X: 0, Y: -100.5, Z: -1}, 100, material.NewLambertian(scene.NewVector(0.7, 0.7, 0))),
+		geometry.NewSphere(r3.Vector{X: -0.6, Y: 0, Z: -1}, 0.5, material.NewFuzziness(scene.NewVector(0.8, 0.1, 0.1), 1.0)),
+		geometry.NewSphere(r3.Vector{X: 0.6, Y: 0, Z: -1}, 0.5, material.NewFuzziness(scene.NewVector(0.3, 0.8, 0.5), 1.0)),
+		geometry.NewSphere(r3.Vector{X: 0, Y: -100.5, Z: -1}, 100, material.NewMetal(scene.NewVector(0.7, 0.7, 0.7))),
 	)
 	result := e.CreateP3Data()
+	fmt.Println(result)
 	e.CreateFile("test.ppm", result.Header, result.Body.String())
 }
--- pkg/image/ppm.go
+++ pkg/image/ppm.go
@@ -67,7 +67,7 @@ func Render(i int, j int, camera camera.Parts, wg *sync.WaitGroup, img *Elements
 	SuperSampling(i, j, camera, &color, geometry.New(img.World), canvas.Color{X: 0.5, Y: 0.7, Z: 1.0}, img)
 	// fix gamma
 	color = scene.NewVector(math.Sqrt(color.X), math.Sqrt(color.Y), math.Sqrt(color.Z))
-	RGBValidation(color, img)
+	SetRGB(color, img)
 	p[i+j*img.X] = fmt.Sprintf(format.Body, img.Color.R, img.Color.G, img.Color.B)
 }
 
@@ -89,31 +89,13 @@ func SuperSampling(i int, j int, camera camera.Parts, color *r3.Vector, world ge
 	*color = color.Mul(1.0 / float64(img.Sampling))
 }
 
-func RGBValidation(color r3.Vector, img *Elements) {
+func SetRGB(color r3.Vector, img *Elements) {
 	// R
-	if int(255.99*color.X) < 256 {
-		img.Color.R = int(255.99 * color.X)
-	} else if int(255.99*color.X) < 0 {
-		img.Color.R = 0
-	} else {
-		img.Color.R = 255
-	}
+	img.Color.R = int(255.99 * color.X)
 
 	// G
-	if int(255.99*color.Y) < 256 {
-		img.Color.G = int(255.99 * color.Y)
-	} else if int(255.99*color.Y) < 0 {
-		img.Color.G = 0
-	} else {
-		img.Color.G = 255
-	}
+	img.Color.G = int(255.99 * color.Y)
 
 	// B
-	if int(255.99*color.Z) < 256 {
-		img.Color.B = int(255.99 * color.Z)
-	} else if int(255.99*color.Z) < 0 {
-		img.Color.B = 0
-	} else {
-		img.Color.B = 255
-	}
+	img.Color.B = int(255.99 * color.Z)
 }
